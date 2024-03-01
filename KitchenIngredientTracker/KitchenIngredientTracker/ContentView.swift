b08727ded565abb8911e4ccc61f5ce162214fed6
Nynika Badam <nbadam2@illinois.edu>
2024-02-29 19:45:23 -0600

    creating project

---
+++ KitchenIngredientTracker/KitchenIngredientTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KitchenIngredientTracker
+//
+//  Created by Nynika Badam on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KitchenIngredientTracker/KitchenIngredientTracker/KitchenIngredientTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenIngredientTrackerApp.swift
+//  KitchenIngredientTracker
+//
+//  Created by Nynika Badam on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct KitchenIngredientTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
